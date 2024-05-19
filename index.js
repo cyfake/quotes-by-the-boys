6fe1111472df8b9fd5470e08b98f2b6b8984708b
cyfake <imcyfake@gmail.com>
2024-05-19 12:33:28 +0800

    Add dynamic retrieval of commands found in /commands folder and the receiving of command interactions

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// Require the .env file with the Bot Token, Client ID and Guild ID
 require("dotenv").config();
 
-// Requiring discord.js classes
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+// Required modules to load command files
+const fs = require("node:fs");
+const path = require("node:path");
+
+// Require the necessary discord.js classes
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 
 // Create new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -13,3 +18,40 @@ client.once(Events.ClientReady, (readyClient) => {
 
 // Log into Discord with token
 client.login(process.env.DISCORD_TOKEN);
+
+client.commands = new Collection();
+
+// Dynamic retrieval of command files
+const commandsFolderPath = path.join(__dirname, "commands");
+const commandsFolder = fs.readdirSync(commandsFolderPath);
+
+for (const file of commandsFolder) {
+  const filePath = path.join(commandsFolderPath, file);
+  const command = require(filePath);
+
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required 'data' or 'execute' property.`
+    );
+  }
+}
+
+// Receive command interactions
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+  }
+});
