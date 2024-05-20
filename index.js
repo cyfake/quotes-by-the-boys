f525c2d88ba4f8f524e0358d37fb293568a21315
cyfake <imcyfake@gmail.com>
2024-05-20 11:30:20 +0800

    Move ClientReady event to events/ready.js

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 // Create new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// Initialisation
-client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
-
 // Log into Discord with token
 client.login(process.env.DISCORD_TOKEN);
 
