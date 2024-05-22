430fbcedd8463424ce5769cd81198608e9335d54
cyfake <imcyfake@gmail.com>
2024-05-23 01:16:25 +0800

    Add dbInit.js file to sync models

---
+++ dbInit.js
@@ -0,0 +1,22 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("database", "username", "password", {
+  host: "localhost",
+  dialect: "sqlite",
+  logging: false,
+  storage: "database.sqlite",
+});
+
+require("./models/Quotes.js")(sequelize, Sequelize.DataTypes);
+require("./models/Users.js")(sequelize, Sequelize.DataTypes);
+
+const force = process.argv.includes("--force") || process.argv.includes("--f");
+
+sequelize
+  .sync({ force })
+  .then(async () => {
+    console.log("All models were synchronised successfully.");
+
+    sequelize.close();
+  })
+  .catch(console.error);
