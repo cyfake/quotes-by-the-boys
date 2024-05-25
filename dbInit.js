ef7ae8a77923afa12a4ef789bd020f81093a0a76
cyfake <imcyfake@gmail.com>
2024-05-25 13:33:35 +0800

    Move database associations to dbObjects.js to keep connection open while using application

--- dbInit.js
+++ dbInit.js
@@ -1,21 +1,10 @@
 const Sequelize = require("sequelize");
-const { DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize("database", "username", "password", {
   host: "localhost",
   dialect: "sqlite",
   logging: false,
   storage: "database.sqlite",
-});
-
-const Quotes = require("./models/Users.js")(sequelize, Sequelize.DataTypes);
-const Users = require("./models/Quotes.js")(sequelize, Sequelize.DataTypes);
-
-Users.belongsTo(Quotes, {
-  foreignKey: {
-    name: "user_id",
-    type: DataTypes.UUID,
-  },
 });
 
 const force = process.argv.includes("--force") || process.argv.includes("--f");
@@ -28,5 +17,3 @@ sequelize
     sequelize.close();
   })
   .catch(console.error);
-
-module.exports = { Quotes, Users };
