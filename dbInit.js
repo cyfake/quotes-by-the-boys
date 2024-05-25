4118640eb83d90e244bbb5080b82929f7681e38c
cyfake <imcyfake@gmail.com>
2024-05-26 02:00:19 +0800

    Move associations from dbObjects.js to dbInit.js

--- dbInit.js
+++ dbInit.js
@@ -1,4 +1,4 @@
-const Sequelize = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize("database", "username", "password", {
   host: "localhost",
@@ -7,14 +7,30 @@ const sequelize = new Sequelize("database", "username", "password", {
   storage: "database.sqlite",
 });
 
-require("./models/Users.js")(sequelize, Sequelize.DataTypes);
-require("./models/Quotes.js")(sequelize, Sequelize.DataTypes);
+const Quotes = require("./models/Quotes.js")(sequelize, Sequelize.DataTypes);
+const Users = require("./models/Users.js")(sequelize, Sequelize.DataTypes);
+
+Quotes.belongsTo(Users, {
+  foreignKey: {
+    name: "user_id",
+    type: DataTypes.UUID,
+  },
+});
+
+Users.hasMany(Quotes, {
+  foreignKey: {
+    name: "user_id",
+    type: DataTypes.UUID,
+  },
+});
 
 const force = process.argv.includes("--force") || process.argv.includes("--f");
 
 sequelize
-  .sync({ force })
+  .authenticate()
   .then(async () => {
+    await Users.sync({ force });
+    await Quotes.sync({ force });
     console.log("All models were synchronised successfully.");
 
     sequelize.close();
