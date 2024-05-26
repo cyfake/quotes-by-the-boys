5533299a2c6b0b50c781a667a4ef4aef31ee58d9
cyfake <imcyfake@gmail.com>
2024-05-26 18:16:06 +0800

    Create command to get random quote

---
+++ commands/randomquote.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { Users, Quotes } = require("../dbObjects.js");
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("database", "username", "password", {
+  host: "localhost",
+  dialect: "sqlite",
+  logging: false,
+  storage: "database.sqlite",
+});
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("randomquote")
+    .setDescription("Get a random quote."),
+  async execute(interaction) {
+    try {
+      Quotes.findOne({
+        order: sequelize.random(),
+      }).then((res) => {
+        const quote = res.dataValues.quote;
+        Users.findOne({
+          where: {
+            user_id: res.dataValues.user_id,
+          },
+        }).then((res) => {
+          const username = res.dataValues.username;
+          return interaction.reply(`"*${quote}*" ~**${username}**`);
+        });
+      });
+    } catch (error) {
+      return interaction.reply(`Something went wrong. ${error}`);
+    }
+  },
+};
