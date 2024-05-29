ebe3bdc0a665d9fd60b4685e50e6870369e31877
cyfake <imcyfake@gmail.com>
2024-05-29 22:52:26 +0800

    Add InteractionCreate Event for autocorrect for /quote command

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -6,24 +6,36 @@ module.exports = {
   name: Events.InteractionCreate,
   async execute(interaction) {
     if (
-      !interaction.isChatInputCommand() &&
-      !interaction.isMessageContextMenuCommand()
-    )
-      return;
+      interaction.isChatInputCommand() ||
+      interaction.isMessageContextMenuCommand()
+    ) {
+      const command = interaction.client.commands.get(interaction.commandName);
 
-    const command = interaction.client.commands.get(interaction.commandName);
+      if (!command) {
+        console.error(
+          `No command matching ${interaction.commandName} was found.`
+        );
+        return;
+      }
+      try {
+        await command.execute(interaction);
+      } catch (error) {
+        console.error(error);
+      }
+    } else if (interaction.isAutocomplete()) {
+      const command = interaction.client.commands.get(interaction.commandName);
 
-    if (!command) {
-      console.error(
-        `No command matching ${interaction.commandName} was found.`
-      );
-      return;
-    }
-
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(error);
+      if (!command) {
+        console.error(
+          `No command matching ${interaction.commandName} was found.`
+        );
+        return;
+      }
+      try {
+        await command.autocomplete(interaction);
+      } catch (error) {
+        console.error(error);
+      }
     }
   },
 };
